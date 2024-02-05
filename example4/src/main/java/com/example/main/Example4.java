aadb14b09b90a553fc302c48f822aab14bdc5fe2
nishchala <154533813+igruffalo@users.noreply.github.com>
2024-02-05 18:54:27 -0500

    @Autowired on setter method for injecting beans

---
+++ example4/src/main/java/com/example/main/Example4.java
@@ -0,0 +1,22 @@
+package com.example.main;
+
+import com.example.beans.Person;
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class Example4 {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Person person = context.getBean(Person.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("person name from Spring context is: " + person.getName());
+        System.out.println("Vehicle name from Spring context is: " + vehicle.getName());
+        System.out.println("Vehicle that Person owns is: " + person.getVehicle());
+    }
+
+}
---
+++ example5/src/main/java/com/example/beans/Person.java
@@ -0,0 +1,38 @@
+package com.example.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    private String name = "Lucy";
+
+    public Person() {
+        System.out.println("Person bean created by Spring.");
+    }
+
+    /*
+    The @Autowired annotation marks on a field, constructor, Setter method is used to auto-wire the beans that is
+    'injecting beans'(Objects) at runtime by Spring Dependency Injection mechanism
+     */
+
+//    @Autowired(required = false)
+
+    private Vehicle vehicle;
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+    @Autowired
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
